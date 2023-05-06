805d4d6a8188acf2652813ec610a6a574b27c1ae
gerikah <aldaygerikah@gmail.com>
2023-05-06 15:40:38 +0800

    pseudocode

---
+++ simple_calculator.py
@@ -0,0 +1,15 @@
+# Object Oriented Programming CMPE-103 PROGRAMMING EXERCISES : Assignment 5
+# ALDAY, Gerikah L. - BSCPE 1-5
+
+# ask the user to choose one of the four math operations
+# exception (valueError)
+# ask the user for two numbers
+# exception (valueError) (zeroDivisionError)
+# display result
+# ask is the user wants to try again or not
+# exception (valueError)
+# if yes:
+    # repeat the first step
+# if no:
+    # display "thank you"
+    # end 
